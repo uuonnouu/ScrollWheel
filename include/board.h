5a81bdffc39b7d822bbbe0f6d266c1d1874873e0
uuon <UUONUUORd@gmail.com>
2026-06-07 16:34:46 +0200

    Update board.h

--- include/board.h
+++ include/board.h
@@ -13,13 +13,16 @@
 // pull-up with switch closing to GND.
 //
 
-#define GPIO_SW1		10
-#define GPIO_SW2		11
-#define GPIO_SW3		12
-#define GPIO_SW4		13
-
-#define I2C_BUS			i2c0
-#define I2C_SDA			4
-#define I2C_SCL			5
+#define I2C_BUS     i2c0
+#define I2C_SDA     20
+#define I2C_SCL     21
+#define GPIO_SW1    0
+#define GPIO_SW2    1
+#define GPIO_SW3    2
+#define GPIO_SW4    3
+// No WS2812_PIN — using dumb RGB
+#define LED_R_PIN   18
+#define LED_G_PIN   19
+#define LED_B_PIN   20
 
 #define WS2812_PIN		1	// WS2812 RGB LED
